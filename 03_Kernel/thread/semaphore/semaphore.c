8674beeadc29d2d86c7c2c34fce22965c8c02e2a
liujiayi10 <liujiayi10@faw.com.cn>
2025-01-15 10:18:16 +0800

    信号量P操作修改

--- 03_Kernel/thread/semaphore/semaphore.c
+++ 03_Kernel/thread/semaphore/semaphore.c
@@ -12,17 +12,17 @@ void semaphore_init(struct semaphore *p_sema, uint32_t value) {
 void semaphore_P(struct semaphore *p_sema) {
     enum interrupt_status old_status = get_interrupt_status();
     interrupt_disable();
-    if (p_sema->value == 0) {
+    // 为什么用 while ：线程唤醒后信号量的值可能还为0，需要再次阻塞
+    while (p_sema->value == 0) {
         struct PCB_INFO* cur_pcb = get_curthread_pcb();
         if (elem_find(&(p_sema->waiters_list), &(cur_pcb->general_tag))) {  // 如果当前线程在等待队列，则不处理
             put_str("\nsemaphore_P error! pcb in waiters list!\n");
-            return;
         }
         list_push(&(p_sema->waiters_list), &(cur_pcb->general_tag));
         thread_block(TASK_BLOCKED);
-    } else {
-        p_sema->value--;
     }
+    // 线程唤醒后在这，此时的信号值为1，需要在此处减1
+    p_sema->value--;
     ASSERT(p_sema->value == 0);
     set_interrupt_status(old_status);
 }
